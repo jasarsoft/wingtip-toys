aeb0a94f72caac83ef7f471917a2b95acaae0a41
Edin Jašarević <edinjasar14@gmail.com>
2018-07-27 14:13:02 +0200

    kontrola za prikaza nivigacioni podataka

--- WingtipToys/Site.Master.designer.cs
+++ WingtipToys/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace WingtipToys {
         protected global::System.Web.UI.WebControls.Image Image1;
         
         /// <summary>
+        /// categoryList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListView categoryList;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
