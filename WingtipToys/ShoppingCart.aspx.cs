b58af53df048d04c3651e2161135017a311d6f63
Edin Jašarević <edinjasar14@gmail.com>
2018-07-27 15:09:32 +0200

    izmjena za korpu

--- WingtipToys/ShoppingCart.aspx.cs
+++ WingtipToys/ShoppingCart.aspx.cs
@@ -13,7 +13,22 @@ namespace WingtipToys
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                decimal cartTotal = 0;
+                cartTotal = usersShoppingCart.GetTotal();
+                if (cartTotal > 0)
+                {
+                    // Display Total.
+                    lblTotal.Text = String.Format("{0:c}", cartTotal);
+                }
+                else
+                {
+                    LabelTotalText.Text = "";
+                    lblTotal.Text = "";
+                    ShoppingCartTitle.InnerText = "Shopping Cart is Empty";
+                }
+            }
         }
 
         public List<CartItem> GetShoppingCartItems()
