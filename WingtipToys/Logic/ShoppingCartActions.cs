0e537b973e5a40ec5e05b8cc5df8042a940e3b21
Edin Jašarević <edinjasar14@gmail.com>
2018-07-27 15:08:37 +0200

    izracun ukupno odabrani prizvoda

--- WingtipToys/Logic/ShoppingCartActions.cs
+++ WingtipToys/Logic/ShoppingCartActions.cs
@@ -81,5 +81,19 @@ namespace WingtipToys.Logic
             return _db.ShoppingCartItems.Where(
                 c => c.CartId == ShoppingCartId).ToList();
         }
+
+        public decimal GetTotal()
+        {
+            ShoppingCartId = GetCartId();
+            // Multiply product price by quantity of that product to get        
+            // the current price for each of those products in the cart.  
+            // Sum all product price totals to get the cart total.   
+            decimal? total = decimal.Zero;
+            total = (decimal?)(from cartItems in _db.ShoppingCartItems
+                               where cartItems.CartId == ShoppingCartId
+                               select (int?)cartItems.Quantity *
+                               cartItems.Product.UnitPrice).Sum();
+            return total ?? decimal.Zero;
+        }
     }
 }
