9402e76c7f6f557c4cddc064abb44518ef469b12
Edin Jašarević <edinjasar14@gmail.com>
2018-07-27 14:00:41 +0200

    osnovna konfiguracija

---
+++ WingtipToys/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Data.Entity;
+using WingtipToys.Models;
+
+
+
+namespace WingtipToys
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the product database.
+            Database.SetInitializer<ProductContext>(new ProductDatabaseInitializer());
+
+            using (var context = new ProductContext())
+            {
+                context.Database.Initialize(false);
+            }
+        }
+    }
+}
