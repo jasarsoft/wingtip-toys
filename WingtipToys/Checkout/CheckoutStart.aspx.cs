8e6da216a4e647db45401375edcef8c4223953f4
Edin Jašarević <edinjasar14@gmail.com>
2018-07-27 19:54:52 +0200

    checkout stranice

--- WingtipToys/Checkout/CheckoutStart.aspx.cs
+++ WingtipToys/Checkout/CheckoutStart.aspx.cs
@@ -11,7 +11,29 @@ namespace WingtipToys.Checkout
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            NVPAPICaller payPalCaller = new NVPAPICaller();
+            string retMsg = "";
+            string token = "";
 
+            if (Session["payment_amt"] != null)
+            {
+                string amt = Session["payment_amt"].ToString();
+
+                bool ret = payPalCaller.ShortcutExpressCheckout(amt, ref token, ref retMsg);
+                if (ret)
+                {
+                    Session["token"] = token;
+                    Response.Redirect(retMsg);
+                }
+                else
+                {
+                    Response.Redirect("CheckoutError.aspx?" + retMsg);
+                }
+            }
+            else
+            {
+                Response.Redirect("CheckoutError.aspx?ErrorCode=AmtMissing");
+            }
         }
     }
 }
